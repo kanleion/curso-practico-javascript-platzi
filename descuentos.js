2f0e93ded0f9f897ebf454e9f6f386436235ed27
kanleion <manu12694@gmail.com>
2021-07-29 10:47:29 -0400

    agregado el ejercicio de precios y descuentos

--- descuentos.js
+++ descuentos.js
@@ -9,10 +9,50 @@ function operationPriceWithDiscount(price, discount) {
     return priceWithDiscount;
 }
 
+function calculePriceDiscount() {
+    const inputPrice = document.getElementById("inputPrice").value;
+    // const inputDiscount = document.getElementById("inputDiscount").value;
+    const inputCoupon = document.getElementById("InputCoupon").value;
 
-// console.log({
-//     originalPrice,
-//     discount,
-//     percentPriceWithDiscount,
-//     priceWithDiscount
-// });
+    const coupons = [{
+            name: "JuanDC_es_Batman",
+            discount: 15,
+        },
+        {
+            name: "pero_no_le_digas_a_nadie",
+            discount: 30,
+        },
+        {
+            name: "es_un_secreto",
+            discount: 25,
+        },
+    ];
+
+    console.log(coupons);
+
+    const userCoupon = coupons.find(isCouponValueValid);
+
+    const isCouponValueValid = function(coupon) {
+        return coupon.name === coupon.value;
+    };
+
+
+
+    if (!userCoupon) {
+        alert("El cupón " + inputCoupon + " no es válido");
+    } else {
+        const descuento = userCoupon.discount;
+        const precioConDescuento = calcularPrecioConDescuento(inputPrice, descuento);
+
+        const resultP = document.getElementById("ResultP");
+        resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+    }
+
+
+
+    // const discountPrice = operationPriceWithDiscount(inputPrice, descuento);
+
+
+    // const priceResult = document.getElementById("priceResult");
+    // priceResult.innerText = `El precio con descuento es: $${discountPrice}`;
+}
